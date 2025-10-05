00259d612bd28d17219a2a1b8e5dab3fd477acc5
Diogo Toporcov <diogotoporcov@gmail.com>
2025-10-05 16:17:53 -0300

    .

--- finder/db/__init__.py
+++ finder/db/__init__.py
@@ -1,4 +1,4 @@
 from .base import Base
-from .models import collection, image, upload_session, user, refresh_token  # import so tables register
+from .models import collection, image, upload_session, user, refresh_token
 
 __all__ = ["Base", "collection", "image", "upload_session", "user", "refresh_token"]
