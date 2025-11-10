9322163f54e13defcfa24e4aa34b3fbc8bc0649e
Diogo Toporcov <diogotoporcov@gmail.com>
2025-11-10 14:24:57 -0300

    - Updated README.md - Added final OpenAPI documentation.

--- src/main.py
+++ src/main.py
@@ -12,5 +12,18 @@ async def lifespan(_app: FastAPI):
     await run_in_threadpool(EmbeddingService.get_instance)
     yield
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    title="Finder v2",
+    description="Finder v2 is a highly efficient image management system designed for organizing, "
+                "storing, and preventing duplicate images in large databases.",
+    version="1.0.0",
+    license_info={
+        "name": "MIT License",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+    lifespan=lifespan,
+    # docs_url=None,
+    # redoc_url=None,
+    # openapi_url=None
+)
 register_routers(app)
